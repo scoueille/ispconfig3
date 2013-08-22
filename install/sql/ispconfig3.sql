c5a0e11f5a3b5c3027e42e119888dae7d152e8b2
mcramer <m.cramer@pixcept.de>
2013-08-22 12:45:31 +0000

    - Added sql table for new cron system, does not hurt if it is executed in stable, too.

--- install/sql/ispconfig3.sql
+++ install/sql/ispconfig3.sql
@@ -1428,6 +1428,21 @@ CREATE TABLE `sys_config` (
 
 -- --------------------------------------------------------
 
+--
+-- Table structure for table `sys_cron`
+--
+
+CREATE TABLE IF NOT EXISTS `sys_cron` (
+  `name` varchar(50) NOT NULL,
+  `last_run` datetime NULL DEFAULT NULL,
+  `next_run` datetime NULL DEFAULT NULL,
+  `running` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+  PRIMARY KEY (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+-- --------------------------------------------------------
+
 -- 
 -- Table structure for table  `sys_datalog`
 -- 
