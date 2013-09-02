24936c29791332c4bf54cf132adaf0754b0e7869
tbrehm <t.brehm@ispconfig.org>
2013-09-02 07:55:43 +0000

    Fixed: Fatal error:  Call to undefined method tform::checkOwnerPermisssions() in /usr/local/ispconfig/interface/lib/classes/plugin_backuplist.inc.php on line 60

--- interface/lib/classes/plugin_backuplist.inc.php
+++ interface/lib/classes/plugin_backuplist.inc.php
@@ -57,7 +57,9 @@ class plugin_backuplist extends plugin_base {
 					
 					//* check if the user is  owner of the parent domain
 					$domain_backup = $app->db->queryOneRecord("SELECT parent_domain_id FROM web_backup WHERE backup_id = ".$backup_id);
-					if(!$app->tform->checkOwnerPermisssions($this->dataRecord["parent_domain_id"])){
+					
+					$get_domain = $app->db->queryOneRecord("SELECT domain_id FROM web_domain WHERE domain_id = ".$app->functions->intval($domain_backup["parent_domain_id"])." AND ".$this->getAuthSQL('u'));
+					if(empty($get_domain) || !$get_domain) {
 						$app->error($app->tform->lng('no_domain_perm'));
 					}
 					
